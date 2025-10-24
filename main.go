896040a80b700c679d90fc3b112f03ecabb3e407
Tom Gollick <thomasgollick@gmail.com>
2025-10-24 12:10:55 +0100

    Finished index page but not fighting demons trying to solve a routing bug, its so simple as well I feel stupid

--- main.go
+++ main.go
@@ -9,12 +9,24 @@ import (
 var tmpl *template.Template
 
 type Principle struct {
-  Title string
-  Desc string
-  Icon string
-} 
+	Title string
+	Desc  string
+	Icon  string
+}
 
-// parse templates on init
+type Project struct {
+	Title string
+	Desc  string
+	Image string
+}
+
+type Data struct {
+	PrincipleData []Principle
+	ProjectData   []Project
+	PageTitle     string
+}
+
+// Parse templates on init
 func init() {
 	t := template.Must(template.ParseGlob("templates/*.html"))
 	t = template.Must(t.ParseGlob("templates/partials/*.html"))
@@ -26,6 +38,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/projects", projectsHandler)
 
 	log.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -50,12 +63,50 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	if err := tmpl.ExecuteTemplate(w, "index.html", principlesData); err != nil {
+	projectData := []Project{
+		{
+			Title: "AI Job Matching Platform",
+			Desc:  "Full-stack AI Job Matching platform using TF-IDF alongside Cosine similarity to compare CV's and jobs.",
+			Image: "/static/placeholder.avif",
+		},
+		{
+			Title: "DSG Home Finance",
+			Desc:  "Production Full-stack web application for my father’s business DSG Home Finance.",
+			Image: "/static/placeholder.avif",
+		},
+		{
+			Title: "Therapeutic LLM",
+			Desc:  "Dissertation project providing rich context to reasoning LLM's for improved therapeutic output.",
+			Image: "/static/placeholder.avif",
+		},
+	}
+
+	data := Data{
+		PrincipleData: principlesData,
+		ProjectData:   projectData,
+		PageTitle:     "Home | Thomas Portfolio",
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "index", data); err != nil {
 		http.Error(w, "Template error: "+err.Error(), http.StatusInternalServerError)
 		log.Println("template error:", err)
 		return
 	}
 	log.Println("Serving index page")
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+	data := Data{
+		PageTitle: "Projects | Thomas Portfolio",
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "projects", data); err != nil {
+		http.Error(w, "Template error: "+err.Error(), http.StatusInternalServerError)
+		log.Println("template error:", err)
+		return
+	}
+
+	log.Println("Serving projects page")
 }
 
 
